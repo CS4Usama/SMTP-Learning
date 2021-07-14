2f618320662f7ec091f8f8f72a05ad8679e2caa3
CS4Usama <cs.cybernetic@gmail.com>
2021-07-14 23:39:23 +0500

    auth

--- auth-navigation/src/navigation/Navigation.jsx
+++ auth-navigation/src/navigation/Navigation.jsx
@@ -13,12 +13,12 @@ export default function Navigation() {
 
             <Switch>
                 <Route exact path="/">
-
-                </Route>
-
-                <Route path="home">
                     <Home />
                 </Route>
+                
+                {/* <Route path="home">
+                    <Home />
+                </Route> */}
 
                 <Route path="login">
                     <Login />
