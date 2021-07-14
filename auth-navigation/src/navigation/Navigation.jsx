ffeed3f0a7562c7c20eadf9ab72672dd2323c996
CS4Usama <cs.cybernetic@gmail.com>
2021-07-14 23:27:45 +0500

    auth

--- auth-navigation/src/navigation/Navigation.jsx
+++ auth-navigation/src/navigation/Navigation.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 export default function Navigation() {
     return(
-        <div>
-            Navigation
-        </div>
+        <Router>
+            <Header />
+            
+            <Footer />
+        </Router>
     );
 }
