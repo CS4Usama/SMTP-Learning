c7670f334e0286da03857116bcf084094c9b922c
CS4Usama <cs.cybernetic@gmail.com>
2021-07-15 00:03:27 +0500

    auth

--- auth-navigation/src/components/Header.jsx
+++ auth-navigation/src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function Header() {
     return(
         <div style={{paddingLeft:50, paddingBottom:50}}>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            <Link style={{paddingRight:30}} to="/">Home</Link>
+            <Link style={{paddingRight:30}} to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
         </div>
     );
