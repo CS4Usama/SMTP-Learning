0222d514486e7f59fc96f09f2430b0df9b70fa24
CS4Usama <cs.cybernetic@gmail.com>
2021-07-14 23:34:18 +0500

    auth

--- auth-navigation/src/navigation/Navigation.jsx
+++ auth-navigation/src/navigation/Navigation.jsx
@@ -1,12 +1,34 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Home from '../modules/home/Home';
+import Login from '../modules/auth/login/Login';
+import Signup from '../modules/auth/signup/Signup';
+
 
 export default function Navigation() {
     return(
         <Router>
             <Header />
-            
+
+            <Switch>
+                <Route exact path="/">
+
+                </Route>
+
+                <Route path="home">
+                    <Home />
+                </Route>
+
+                <Route path="login">
+                    <Login />
+                </Route>
+
+                <Route path="signup">
+                    <Signup />
+                </Route>
+            </Switch>
+
             <Footer />
         </Router>
     );
