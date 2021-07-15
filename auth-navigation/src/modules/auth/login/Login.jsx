03d6a236f71ac8c4c20ae2c0454431c2d69c7498
CS4Usama <cs.cybernetic@gmail.com>
2021-07-15 12:15:53 +0500

    auth

--- auth-navigation/src/modules/auth/login/Login.jsx
+++ auth-navigation/src/modules/auth/login/Login.jsx
@@ -1,7 +1,9 @@
 export default function Login() {
     return(
         <div>
-            Login
+            <input type="text" placeholder="Enter User Name" />
+            <input type="password" placeholder="Enter Your Password" />
+            <button onClick={}>Submit</button>
         </div>
     );
 }
