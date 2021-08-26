c4a9615f87db2cfc4f6b411abaf0d8e1ef214b95
Cyber Hawk <cs.cybernetic@gmail.com>
2021-08-26 23:38:01 +0500

    cors, body-parser testing

---
+++ Backend/class-02/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// const port = 3000;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+    req.clientData;
+    // Logics of Main Server - When a GET Request is made to the Homepage
+    res.send('Server is Working...');
+});
+
+app.get('/login', function(req, res) {
+    // Logics of Login
+    let resBack = {userID:'123', email:'test@gmail.com'};
+    res.status(200).json(resBack);
+});
+
+app.post('/signup', function(req, res) {
+    console.log(req.body);
+    // Logics of Signup
+    res.json({name: req.body.name})
+});
+
+app.get('/forget', function(req, res) {
+    // Logics
+    res.send("HELLO WORLD");
+});
+
+
+// IP:5000/login
+
+app.listen('5000', (err) => {
+
+    if(err) {
+        console.log("Something went Wrong", error);
+    }
+
+});
+
+
+// app.listen(port, () => {
+//     console.log(`App Listening at http://localhost:${port}`)
+// });
