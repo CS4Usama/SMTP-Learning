25ffa39d561e235ac6fafd72340b455f4b32f908
Cyber Hawk <cs.cybernetic@gmail.com>
2021-09-05 11:04:17 +0500

    jwt auth

--- Backend/class-04/components/auth/AuthController.js
+++ Backend/class-04/components/auth/AuthController.js
@@ -1,9 +1,13 @@
 const authModel = require('./AuthModel');
 
 module.exports.signup = (req, res) => {
-    const newOrder = new authModel({
+    if(!name || !email || !password || !createdAt) {
+        res.status(400).send("All Parameters (Params) are Required");
+    }
+    const newUser = new authModel({
         name: req.body.name,
         email: req.body.email,
+        password: req.body.password,
         createdAt: new Date(),
     });
 
@@ -15,30 +19,4 @@ module.exports.signup = (req, res) => {
         }
         res.send("Order added Successfully");
     });
-}
-
-module.exports.getOrder = (req, res) => {
-    ordersModel.find({}, (err, data) => {
-        console.log("SUCCESS", data);
-        console.log("ERROR", err);
-        if(err) {
-            res.send("Some ERROR Occurs in Get Orders");
-        }
-        res.json(data);
-    });
-}
-
-module.exports.updateOrder = (req, res) => {
-    ordersModel.updateOne(
-        { date: "Some Date" },
-        { createdAt: new Date() },
-        (err, data) => {
-            console.log("success", data);
-            console.log("error", err);
-            if(err) {
-                res.send("Error Occurs while Updating an Order");
-            }
-            res.json(data);
-        }
-    );
 }
