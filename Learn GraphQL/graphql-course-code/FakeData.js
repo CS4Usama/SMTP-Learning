597dad620c99accb289ed9642f2ba3244d88dd03
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-17 20:46:27 +0500

    learning graphql

---
+++ Learn GraphQL/graphql-course-code/FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+    {
+        id: 1,
+        name: 'Usama',
+        userName: 'CS4Usama',
+        age: 21,
+        nationality: 'Pakistan',
+    },
+    {
+        id: 2,
+        name: 'ALi',
+        userName: 'someName',
+        age: 25,
+        nationality: 'USA',
+    },
+    {
+        id: 3,
+        name: 'My_Name',
+        userName: 'your_name',
+        age: 30,
+        nationality: 'Turkey',
+    },
+    {
+        id: 4,
+        name: 'John Joe',
+        userName: 'joe123',
+        age: 28,
+        nationality: 'Canada',
+    },
+    {
+        id: 5,
+        name: 'Robert Don',
+        userName: 'don-robert',
+        age: 34,
+        nationality: 'United States',
+    },
+];
+
+module.exports = { UserList };
