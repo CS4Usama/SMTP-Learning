ca659b763f458543cb33521ef2e867134bcd77f3
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-19 09:32:44 +0500

    resolvers

--- Learn GraphQL/graphql-course-code/FakeData.js
+++ Learn GraphQL/graphql-course-code/FakeData.js
@@ -4,7 +4,23 @@ const UserList = [
         name: 'Usama',
         userName: 'CS4Usama',
         age: 21,
-        nationality: 'Pakistan',
+        nationality: 'PAKISTAN',
+        friends: [
+            {
+                id: 2,
+                name: 'ALi',
+                userName: 'someName',
+                age: 25,
+                nationality: 'USA',
+            },
+            {
+                id: 3,
+                name: 'My_Name',
+                userName: 'your_name',
+                age: 30,
+                nationality: 'TURKEY',
+            },
+        ],
     },
     {
         id: 2,
@@ -18,22 +34,59 @@ const UserList = [
         name: 'My_Name',
         userName: 'your_name',
         age: 30,
-        nationality: 'Turkey',
+        nationality: 'TURKEY',
     },
     {
         id: 4,
         name: 'John Joe',
         userName: 'joe123',
         age: 28,
-        nationality: 'Canada',
+        nationality: 'CANADA',
+        friends: [
+            {
+                id: 4,
+                name: 'John Joe',
+                userName: 'joe123',
+                age: 28,
+                nationality: 'CANADA',
+            },
+        ],
     },
     {
         id: 5,
         name: 'Robert Don',
         userName: 'don-robert',
         age: 34,
-        nationality: 'United States',
+        nationality: 'AUSTRALIA',
     },
 ];
 
-module.exports = { UserList };
+
+const MovieList = [
+    {
+        id: 1,
+        name: "Avengers Endgame",
+        yearOfPublication: 2019,
+        isInTheaters: true,
+    },
+    {
+        id: 2,
+        name: "Interstellar",
+        yearOfPublication: 2007,
+        isInTheaters: true,
+    },
+    {
+        id: 3,
+        name: "Superbad",
+        yearOfPublication: 2009,
+        isInTheaters: true,
+    },
+    {
+        id: 4,
+        name: "PedroTech The Movie",
+        yearOfPublication: 2035,
+        isInTheaters: false,
+    },
+];
+
+module.exports = { UserList, MovieList };
--- Learn GraphQL/graphql-course-code/schema/resolvers.js
+++ Learn GraphQL/graphql-course-code/schema/resolvers.js
@@ -1,11 +1,37 @@
-const { UserList } = require('../FakeData');
+const { UserList, MovieList } = require('../FakeData');
+const _ = require('lodash');
 
 const resolvers = {
     Query: {
-        users() {
+        // USER RESOLVERS
+        // users() {
+        users: () => {
             return UserList;
         },
+        user: (parent, args) => {
+            
+            const id = args.id;
+            const user = _.find(UserList, { id: Number(id) });
+            return user;
+        },
+
+        // USER RESOLVERS
+        movies: () => {
+            return MovieList
+        },
+        movie: (parent, args) => {
+            const name = args.name;
+            const movie = _.find(MovieList, { name: name });
+            // const movie = MovieList.find(movie => movie.name === name);
+            return movie;
+        },
     },
+
+    User: {
+        favoriteMovies: () => {
+            return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010);
+        },
+    }
 };
 
 module.exports = { resolvers };
