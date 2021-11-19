c182dcd729274dc0fe3127a32a1380e9ea7d770e
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-19 12:17:34 +0500

    setup ApolloClient

--- Learn GraphQL/graphql-course-code/client/src/App.js
+++ Learn GraphQL/graphql-course-code/client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 
 function App() {
+    const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        // uri: LINK_OF_GRAPHQL_API,    Connect API and Start Working... (Communication)
+        uri: 'http://localhost:3001/graphql',
+    });
+
     return(
-        <div className='App'>
-            Hello World
-        </div>
+        <ApolloProvider client={client}>
+            <div className='App'>
+                <h1>
+                    Hello World
+                </h1>
+            </div>
+        </ApolloProvider>
     );
 }
 
