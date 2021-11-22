ea110248bb1f6193001118c84f4ad52687c60161
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-23 00:32:53 +0500

    route linking

--- Learn Next.js/thapa-nextjs-learning/pages/index.js
+++ Learn Next.js/thapa-nextjs-learning/pages/index.js
@@ -1,9 +1,15 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello World</h1>
+      {/* Simple Linking or Routing using Anchor Tag */}
+      {/* <a href='/'></a> */}
+      <a href='/blog/further'>Simple Link </a>
+      {/* Next.js Routing */}
+      <Link href='/blog/further'> Next.js Route</Link>
     </div>
   )
 }
