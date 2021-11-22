d0def7e1f69c691d76c63cb9d58abb8ad7a056ec
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-23 00:54:40 +0500

    404 custom error page

--- Learn Next.js/thapa-nextjs-learning/pages/index.js
+++ Learn Next.js/thapa-nextjs-learning/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <h1>Hello World</h1>
       {/* Simple Linking or Routing using Anchor Tag */}
-      {/* <a href='/'></a> */}
+      {/* <a href='/'>Home</a> */}
       <a href='/blog/further'>Simple Link </a>
       {/* Next.js Routing */}
       <Link href='/blog/further'> Next.js Route</Link>
