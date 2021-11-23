5ca763c38b05a3c2aa217d7038001b93fb33c798
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-23 07:12:18 +0500

    jsx

--- Learn Next.js/thapa-nextjs-learning/pages/404.js
+++ Learn Next.js/thapa-nextjs-learning/pages/404.js
@@ -20,6 +20,14 @@ const ErrorPage = () => {
 
     return(
         <div>
+            <style>
+                {`
+                    a {
+                        color: blue;
+                    }
+                `}
+            </style>
+
             <center>404 Error Page</center>
             <a onClick={() => router.push('/')} className='custom-home-error-style' style={{color: 'tomato'}}>Home </a>
             <a onClick={ctaHandler}> Home2 Handler</a>
