96e967365785a86748451218b6d96bd182e6b706
Cyber Hawk <cs.cybernetic@gmail.com>
2021-11-23 07:34:37 +0500

    Image Component

--- Learn Next.js/thapa-nextjs-learning/pages/index.js
+++ Learn Next.js/thapa-nextjs-learning/pages/index.js
@@ -1,6 +1,8 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import SomeStyleTestingComp from '../components/SomeComp';
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <a href='/blog/further'>Simple Link </a>
       {/* Next.js Routing */}
       <Link href='/blog/further'> Next.js Route</Link>
+      <Image src='/favicon.ico' alt='some image' width='100' height='100' />
+      <Image src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='some image' width='100' height='100' />
     </div>
   )
 }
