d9d690fea888eb8e9aa4a3909cb0bf3d7f31658a
Cyber Hawk <cs.cybernetic@gmail.com>
2022-09-01 00:46:46 +0500

    Delete Learn GraphQL directory

--- Learn GraphQL/graphql-server/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send('Up and Running with GraphQL');
-});
-
-app.listen(5000, () => console.log('App Running at 5000 Port'));
--- Learn GraphQL/graphql-server/src/db.js
+++
@@ -1,4 +0,0 @@
-export const users = [
-    { id: 1, name: 'Usama', email: 'cs.cybernetic@gmail.com', age: 21 },
-    { id: 2, name: 'Person', email: 'person@domain.com', age: 25 }
-];
